b3cc88f29bcca0435ede385559cd2329a81d02b0
lvpin284 <lvpin0220@gmail.com>
2023-11-11 20:34:19 +0800

    第四次修改

--- ICMPPing.py
+++ ICMPPing.py
@@ -13,6 +13,9 @@ import binascii
 ICMP_ECHO_REQUEST = 8 #ICMP type code for echo request messages
 ICMP_ECHO_REPLY = 0 #ICMP type code for echo reply messages
 
+ICMP_Type_Unreachable = 11  # unacceptable host
+ICMP_Type_Overtime = 3  # request overtime
+
 ID = 0  # ID of icmp_header
 SEQUENCE = 0  # sequence of ping_request_msg
 
@@ -43,11 +46,33 @@ def checksum(string):
 	
 def receiveOnePing(icmpSocket, destinationAddress, ID, timeout):
 	# 1. Wait for the socket to receive a reply
+	timeBeginReceive = time.time()
+	whatReady = select.select([icmpSocket], [], [], timeout)
+	timeInRecev = time.time() - timeBeginReceive
+	if not whatReady[0]:
+		return -1
+	timeReceived = time.time()
 	# 2. Once received, record time of receipt, otherwise, handle a timeout
+	recPacket, addr = icmpSocket.recvfrom(1024)
 	# 3. Compare the time of receipt to time of sending, producing the total network delay
+	byte_in_double = struct.calcsize("!d")
+	timeSent = struct.unpack("!d", recPacket[28: 28 + byte_in_double])[0]
+	totalDelay = timeReceived - timeSent
 	# 4. Unpack the packet header for useful information, including the ID
+	rec_header = recPacket[20:28]
+	replyType, replyCode, replyCkecksum, replyId, replySequence = struct.unpack('!bbHHh', rec_header)
 	# 5. Check that the ID matches between the request and reply
-	# 6. Return total network delay
+	if ID == replyId and replyType == ICMP_ECHO_REPLY:
+		# 6. Return total network delay
+		return totalDelay
+	elif timeInRecev > timeout or replyType == ICMP_Type_Overtime:
+		return -3  # ttl overtime/timeout
+	elif replyType == ICMP_Type_Unreachable:
+		return -11  # unreachable
+	else:
+		print("request over time")
+		return -1
+
 	pass # Remove/replace when function is complete
 	
 def sendOnePing(icmpSocket, destinationAddress, ID):
@@ -72,11 +97,20 @@ def doOnePing(destinationAddress, timeout):
 	# 2. Call sendOnePing function
 	sendOnePing(icmp_Socket, destinationAddress, ID)
 	# 3. Call receiveOnePing function
+	totalDelay = receiveOnePing(icmp_Socket, ID, timeout)
 	# 4. Close ICMP socket
+	icmp_Socket.close()
 	# 5. Return total network delay
+	return totalDelay
 	pass # Remove/replace when function is complete
 	
 def ping(host, timeout=1):
+	send = 0
+	lost = 0
+	receive = 0
+	maxTime = 0
+	minTime = 1000
+	sumTime = 0
 	# 1. Look up hostname, resolving it to an IP address
 	destinationIp = socket.gethostbyname(host)
 	global ID
@@ -85,10 +119,45 @@ def ping(host, timeout=1):
 		global SEQUENCE
         SEQUENCE = i
 	# 2. Call doOnePing function, approximately every second
-	# 3. Print out the returned delay
-	# 4. Continue this process until stopped	
+	delay = doOnePing(destinationIp, timeout) * 1000
+	send += 1
+	if delay > 0:
+		receive += 1
+		if maxTime < delay:
+			maxTime = delay
+		if minTime > delay:
+			minTime = delay
+		sumTime += delay
+		# 3. Print out the returned delay
+		print("Receive from: " + str(destinationIp) + ", delay = " + str(int(delay)) + "ms")
+	else:
+		# 测量和报告包丢失，包括无法到达的目的地
+		lost += 1
+		print("Fail to connect. ", end="")
+		# 处理不同的ICMP错误码，如目的主机不可达、目的网络不可达
+		if delay == -11:
+			# type = 11, target unreachable
+			print("Target net/host/port/protocol is unreachable.")
+		elif delay == -3:
+			# type = 3, ttl overtime
+			print("Request overtime.")
+		else:
+			# otherwise, overtime
+			print("Request overtime.")
+	time.sleep(1)
+	# 4. Continue this process until stopped
+	if receive != 0:
+		avgTime = sumTime / receive
+		recvRate = receive / send * 100.0
+		print(
+			"\nSend: {0}, success: {1}, lost: {2}, rate of success: {3}%.".format(send, receive, lost, recvRate))
+		print(
+			# 一旦停止，显示所有测量值的最小、平均和最大延迟
+			"MaxTime = {0}ms, MinTime = {1}ms, AvgTime = {2}ms".format(int(maxTime), int(minTime), int(avgTime)))
+	else:
+		print("\nSend: {0}, success: {1}, lost: {2}, rate of success: 0.0%".format(send, receive, lost))
+
 	pass # Remove/replace when function is complete  
-	
+
 
 ping("lancaster.ac.uk")
-
