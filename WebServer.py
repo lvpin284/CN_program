9a91ef7d36f33d64e44e1059f9b1c1a7c50e5aaf
lvpin284 <lvpin0220@gmail.com>
2023-11-11 21:44:39 +0800

    第六次修改

--- WebServer.py
+++ WebServer.py
@@ -3,6 +3,7 @@
 
 import socket
 import sys
+import threading
 
 
 def handleRequest(tcpSocket):
@@ -17,11 +18,37 @@ def handleRequest(tcpSocket):
 
 def startServer(serverAddress, serverPort):
 	# 1. Create server socket
+	serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	# 2. Bind the server socket to server address and server port
+	serverSocket.bind((serverAddress, serverPort))
 	# 3. Continuously listen for connections to server socket
+	serverSocket.listen(0)
 	# 4. When a connection is accepted, call handleRequest function, passing new connection socket (see https://docs.python.org/3/library/socket.html#socket.socket.accept)
+	while True:
+		try:
+			print("wait for connecting...")
+			print("while true")
+			tcpSocket, clientAddr = serverSocket.accept()
+			print("one connection is established, ", end="")
+			print("address is: %s" % str(clientAddr))
+			handleThread = threading.Thread(target=handleRequest, args=(tcpSocket,))
+			handleThread.start()
+		except Exception as err:
+			print(err)
+			break
 	# 5. Close server socket
+	serverSocket.close()
 	pass # Remove/replace when function is complete
 
+if __name__ == '__main__':
+    while True:
+        try:
+            hostPort = int(input("Input the port you want: "))
+            startServer("", hostPort)
+            break
+        except Exception as e:
+            print(e)
+            continue
 
-startServer("", 8000)
+#startServer("", 8000)
--- traceroute.py
+++ traceroute.py
@@ -7,3 +7,22 @@ import time
 import select
 import socket
 
+
+def check_sum(strings):
+    csum = 0
+    countTo = (len(strings) / 2) * 2
+    count = 0
+    while count < countTo:
+        thisVal = strings[count + 1] * 256 + strings[count]
+        csum = csum + thisVal
+        csum = csum & 0xffffffff
+        count = count + 2
+    if countTo < len(strings):
+        csum = csum + strings[len(strings) - 1]
+        csum = csum & 0xffffffff
+    csum = (csum >> 16) + (csum & 0xffff)
+    csum = csum + (csum >> 16)
+    answer = ~csum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer
